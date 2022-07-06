5c0f73759686e09c85fa29e70633767932ef3bde
gaizkadev <gaizkadev@gmail.com>
2022-07-06 21:40:19 +0200

    images

--- components/header.js
+++ components/header.js
@@ -2,11 +2,22 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h2 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight md:pr-8">
       <Link href="/">
-        <a className="hover:underline">Blog</a>
-      </Link>
-      .
-    </h2>
+        <a className="hover:underline">home.</a>
+      </Link>      
+      </h2>
+      <h2 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight md:pr-8">
+        <Link href="/about">
+          <a className="hover:underline">about.</a>
+        </Link>
+      </h2>
+      <h2 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight md:pr-8">
+        <Link href="/gaizka">
+          <a className="hover:underline">gaizka.</a>
+        </Link>
+      </h2>
+    </section>
   )
 }
