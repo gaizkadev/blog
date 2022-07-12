5b90377e8d43e9dca243962a1f13215c5dd13b60
gaizkadev <gaizkadev@gmail.com>
2022-07-12 11:45:21 +0200

    footer

--- components/footer.js
+++ components/footer.js
@@ -1,65 +1,69 @@
 import Container from "./container";
-import { EXAMPLE_PATH } from "../lib/constants";
 import Image from "next/image";
+import Html from '../public/assets/icons/html.png'
+import Css from '../public/assets/icons/css.png'
+import React from '../public/assets/icons/react.png'
+import Next from '../public/assets/icons/next.png'
+import Svelte from '../public/assets/icons/svelte.png'
+import Github from '../public/assets/icons/github.png'
+
+
 
 export default function Footer() {
   return (
     <Container>
       <footer className=" border-t-2 border-gray-800">
-        <div className="py-8 flex flex-col lg:flex-row items-center">
-          <h3 className="text-xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2 text-gray-800">
-            <span>
-              <Image
-                src="/assets/logos/html.png"
-                alt="Html 5"
-                width={32}
-                height={32}
-              />
-            </span>
-            <span>
-              <Image
-                src="/assets/logos/css.png"
-                alt=" Css 3"
-                width={32}
-                height={32}
-              />
-            </span>
-            <span>
-              <Image
-                src="/assets/logos/react.png"
-                alt=" React Js"
-                width={32}
-                height={32}
-              />
-            </span>
-            <span>
-              <Image
-                src="/assets/logos/next.png"
-                alt="Next Js"
-                width={32}
-                height={32}
-              />
-            </span>
-            <span>
-              <Image
-                src="/assets/logos/svelte.png"
-                alt="Svelte"
-                width={32}
-                height={32}
-              />
-            </span>
-          </h3>
-
-          <div className="flex flex-col lg:flex-row justify-end items-center lg:pl-4 lg:w-1/2 text-gray-800">
-
-            <a
-              href={`https://github.com/gaizkadev`}
-              className="mx-3 font-bold hover:underline"
-            >
-              GitHub
-            </a>
-          </div>
+        <div className="py-8 flex items-center sm:justify-center">
+          <span>
+            <Image
+              src={Html}
+              alt="Html 5"
+              width={32}
+              height={32}
+            />
+          </span>
+          <span>
+            <Image
+              src={Css}
+              alt=" Css 3"
+              width={32}
+              height={32}
+            />
+          </span>
+          <span>
+            <Image
+              src={React}
+              alt=" React Js"
+              width={32}
+              height={32}
+            />
+          </span>
+          <span>
+            <Image
+              src={Next}
+              alt="Next Js"
+              width={32}
+              height={32}
+            />
+          </span>
+          <span>
+            <Image
+              src={Svelte}
+              alt="Svelte"
+              width={32}
+              height={32}
+            />
+          </span>
+          <span>
+            <Image
+              src={Github}
+              alt="Github"
+              width={32}
+              height={32}
+            />
+          </span>
         </div>
+
       </footer >
     </Container>
 
